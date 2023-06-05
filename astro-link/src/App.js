fabf116598ebd1f36779a1e5173186a2b38ecb00
Metoděj Janota <63124677+janx-xyz@users.noreply.github.com>
2023-06-05 20:35:28 +0200

    First

---
+++ astro-link/src/App.js
@@ -0,0 +1,13 @@
+//IMPORT COMPONENTS
+import Navigation from "./components/Navigation";
+
+//APP
+function App() {
+  return (
+    <div className="App">
+      <Navigation />
+    </div>
+  );
+}
+
+export default App;
---
+++ astro-link/src/index.js
@@ -0,0 +1,14 @@
+//REACT
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+
+//BOOTSTRAP
+import 'bootstrap/dist/css/bootstrap.css';
+
+//APP
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <App />
+);
