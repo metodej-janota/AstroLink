28f26b02f6cee109fcd13bb930d285ba4505396c
Metoděj Janota <63124677+janx-xyz@users.noreply.github.com>
2023-06-05 20:57:03 +0200

    Update

--- astro-link/src/components/Navigation.js
+++ astro-link/src/components/Navigation.js
@@ -1,9 +1,70 @@
-import React from 'react'
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from "react";
 
 function Navigation() {
   return (
-    <div>Navigation</div>
-  )
+    <>
+      <nav className="navbar fixed-top navbar-expand-lg bg-body-tertiary">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">
+            AstroLink
+          </a>
+
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="">
+                  Home
+                </a>
+              </li>
+
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="">
+                  Nevim
+                </a>
+              </li>
+
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="">
+                  Swag
+                </a>
+              </li>
+
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="">
+                  Drip
+                </a>
+              </li>
+            </ul>
+
+            <form className="d-flex" role="search">
+              <input
+                className="form-control me-2"
+                type="search"
+                placeholder="Search"
+                aria-label="Search"
+              ></input>
+              <button className="btn btn-outline-primary" type="submit">
+                Search
+              </button>
+            </form>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
 }
 
-export default Navigation
+export default Navigation;
--- astro-link/src/index.js
+++ astro-link/src/index.js
@@ -4,8 +4,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-//BOOTSTRAP
-import 'bootstrap/dist/css/bootstrap.css';
+// Bootstrap CSS
+import "bootstrap/dist/css/bootstrap.min.css";
+// Bootstrap Bundle JS
+import "bootstrap/dist/js/bootstrap.bundle.min";
 
 //APP
 const root = ReactDOM.createRoot(document.getElementById('root'));
